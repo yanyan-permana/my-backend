fd8ad9b70208ea056011f1bf5a9e0cb2ffd56bad
asephidayat <asephidayat366@gmail.com>
2023-08-24 09:09:40 +0700

    coba middleware cors

---
+++ app/Http/Middleware/AddCorsHeaders.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class AddCorsHeaders
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (strpos($request->url(), '/public/uploads/') !== false) {
+            $response = $next($request);
+
+            $response->header('Access-Control-Allow-Origin', '*'); // Ganti dengan domain yang sesuai
+            $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+            $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+            return $response;
+        }
+        return $next($request);
+    }
+}
