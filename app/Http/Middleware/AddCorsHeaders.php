406cbe192006ab08d63e40cbe84c4e7dbf0446f7
asephidayat <asephidayat366@gmail.com>
2023-08-31 17:01:40 +0700

    tambah cors

--- app/Http/Middleware/AddCorsHeaders.php
+++ app/Http/Middleware/AddCorsHeaders.php
@@ -20,7 +20,7 @@ class AddCorsHeaders
         $response = $next($request);
 
         $response->header('Access-Control-Allow-Origin', '*'); // Ganti dengan domain yang sesuai
-        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
         return $response;
