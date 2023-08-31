fb38e979e9d7737fe585b6b1235ef5a3c5f3dc00
asephidayat <asephidayat366@gmail.com>
2023-08-31 17:03:41 +0700

    cors

--- app/Http/Middleware/AddCorsHeaders.php
+++ app/Http/Middleware/AddCorsHeaders.php
@@ -17,14 +17,16 @@ class AddCorsHeaders
     public function handle(Request $request, Closure $next)
     {
         // if (strpos($request->url(), '/public/uploads/') !== false) {
-        $response = $next($request);
+        /*  $response = $next($request);
 
         $response->header('Access-Control-Allow-Origin', '*'); // Ganti dengan domain yang sesuai
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-        return $response;
+        return $response; */
         // }
-        // return $next($request);
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
